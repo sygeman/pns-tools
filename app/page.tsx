a6bb6524c5a1549b9a772844462485c53c7ac1fa
Sygeman <sygeman@gmail.com>
2025-01-29 20:25:29 +0300

    upd

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,15 @@ import { MapView } from "./map-view";
 
 export default function Home() {
   return (
-    <div className="absolute size-full flex">
+    <div className="absolute size-full">
       <div className="relative overflow-hidden">
         <MapView />
       </div>
-      {/* <div className="w-90">Tools</div> */}
+      <div className="absolute bottom-0 w-full flex justify-center items-center backdrop-blur-xs bg-black/50 h-20">
+        <div className="h-10 px-4 backdrop-blur-xs bg-[#333173] rounded-sm flex justify-center items-center">
+          ШК
+        </div>
+      </div>
     </div>
   );
 }
